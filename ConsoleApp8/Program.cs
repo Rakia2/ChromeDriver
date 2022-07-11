d4d3b14905ad7568446e86940365947dce937a57
Vostro 14 <Vostro 14@DESKTOP-9U24DL8>
2022-07-11 19:17:34 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DriverManagement
+{
+
+    public class Program
+    {
+
+    
+
+
+
+        public static void Main() {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Console.WriteLine("Testing...");
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
